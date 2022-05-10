509ec530e7eeca0a9c942950576bab7f7d5ad820
Agustin Lapenna <agustinjlapenna@gmail.com>
2022-05-09 22:24:05 -0300

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+    
+  const [categories, setCategories] = useState(["One Punch"]);
+  
+    // const handleAdd = () => {
+    //     setCategories([...categories, "Avatar"]);
+    //     // setCategories( cats => [...cats, "Avatar"]);
+    // };
+
+  return (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setCategories={ setCategories }/>
+        <hr />
+
+        <ol>
+            {
+                categories.map( cat => 
+                    <GifGrid 
+                        key={cat}
+                        category={cat} 
+                    />
+                )
+            }
+        </ol>
+    </>
+  )
+}
